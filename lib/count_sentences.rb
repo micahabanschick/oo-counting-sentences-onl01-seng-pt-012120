c02f162176df958fed32d1c6e0277eae08037deb
Micah Banschick <micha.ban@gmail.com>
2020-01-31 15:38:14 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self[self.length - 1] == "." ? true : false   
   end
 
   def question?
-
+    self[self.length - 1] == "?" ? true : false
   end
 
   def exclamation?
-
+    self[self.length - 1] == "!" ? true : false
   end
 
   def count_sentences
-
+    self.split(/[.?!]/).delete_if{|elem| elem.length == 0}.length 
   end
+  #binding.pry
 end
+#binding.pry
