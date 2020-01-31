114e24ff807be50841db723d8600f80acbf083c4
Micah Banschick <micha.ban@gmail.com>
2020-01-31 14:45:06 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self[self.length - 1] == "." ? true : false   
   end
 
   def question?
-
+    self[self.length - 1] == "?" ? true : false
   end
 
   def exclamation?
-
+    self[self.length - 1] == "!" ? true : false
   end
 
   def count_sentences
-
+    arr1 = self.split(". ")
+    arr2 = self.split("? ")
+    arr3 = self.split("! ")
+    arr = arr1 + arr2 + arr3
+    arr.length
   end
 end
